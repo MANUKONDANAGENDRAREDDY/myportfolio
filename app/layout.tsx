761866df17ad329dc00c6f105e9757ce0a17a406
nagendra <manukondanagendrareddy@gmail.com>
2025-07-28 13:36:45 +0530

    Initial commit - My working portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import localFont from "next/font/local";
+import "./styles/globals.css";
+import { Jost } from "next/font/google";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "My Portfolio",
+  description: "Portfolio built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${jost.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
